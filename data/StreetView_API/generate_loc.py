86737bfe6b638bd63ffe5e4cf7d48274dd58d554
Kostis-S-Z <Kostis-S-Z@users.noreply.github.com>
2021-09-12 14:31:56 +0200

    Update generate_loc.py

--- data/StreetView_API/generate_loc.py
+++ data/StreetView_API/generate_loc.py
@@ -1,34 +1,48 @@
 import numpy as np
 import pandas as pd
+import os
 
 # Bounding box coordinates from within athens
 # https://boundingbox.klokantech.com/
 
-min_lat = 37946894
-max_lat = 38092677
-min_lon = 23665374
-max_lon = 23926643
+params_def = {
+    "min_lat": 37946894,
+    "max_lat": 38092677,
+    "min_lon": 23665374,
+    "max_lon": 23926643,
+    "method": 'random',  # random or even
+    "n_locs": 1_000  # approximate for even
+}
 
-method = 'random'  # random or even
-n_locs = 10_000  # approximate for even
 
+def create_file(params=params_def):
+    methods = {'random': randomly_generate(params), 'even': evenly_generate(params)}
+    latitudes, longitudes = methods[params['method']]
+    print(f'Generated {len(latitudes)} latitudes, {len(longitudes)} longitudes. '
+        f'Creating {len(latitudes)*len(longitudes)} points.')
+    locations = generate_coordinates(latitudes, longitudes)
+    locs_df = pd.DataFrame(locations, columns=['Address', 'City', 'Country', 'lat', 'long'])
+    print(locs_df.shape)
+    filename = f"athens_{params['method']}.csv"
+    locs_df.to_csv(filename, index=False)
+    return os.getcwd() + '/' + filename
 
-def evenly_generate():
-    n_points = n_locs ** 0.5
-    lat_range = max_lat - min_lat
-    lon_range = max_lon - min_lon
+def evenly_generate(params):
+    n_points = params['n_locs'] ** 0.5
+    lat_range = params['max_lat'] - params['min_lat']
+    lon_range = params['max_lon'] - params['min_lon']
     lat_step_size = lat_range // n_points
     lon_step_size = lon_range // n_points
 
-    lats = np.arange(min_lat, max_lat, lat_step_size)
-    lons = np.arange(min_lon, max_lon, lon_step_size)
+    lats = np.arange(params['min_lat'], params['max_lat'], lat_step_size)
+    lons = np.arange(params['min_lon'], params['max_lon'], lon_step_size)
 
     return lats, lons
 
 
-def randomly_generate():
-    lats = np.random.uniform(min_lat, max_lat, int(n_locs ** 0.5))
-    lons = np.random.uniform(min_lon, max_lon, int(n_locs ** 0.5))
+def randomly_generate(params):
+    lats = np.random.uniform(params['min_lat'], params['max_lat'], int(params['n_locs'] ** 0.5))
+    lons = np.random.uniform(params['min_lon'], params['max_lon'], int(params['n_locs'] ** 0.5))
     return lats, lons
 
 
@@ -46,13 +60,3 @@ def generate_coordinates(lats, lons):
             locs['lat'].append(lat_i)
             locs['long'].append(lon_i)
     return locs
-
-
-methods = {'random': randomly_generate(), 'even': evenly_generate()}
-latitudes, longitudes = methods[method]
-print(f'Generated {len(latitudes)} latitudes, {len(longitudes)} longitudes. '
-      f'Creating {len(latitudes)*len(longitudes)} points.')
-locations = generate_coordinates(latitudes, longitudes)
-locs_df = pd.DataFrame(locations, columns=['Address', 'City', 'Country', 'lat', 'long'])
-print(locs_df.shape)
-locs_df.to_csv(f'athens_{method}.csv', index=False)
