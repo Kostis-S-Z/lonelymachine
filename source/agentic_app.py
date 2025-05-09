0a05ec68bb3aec4afd9832de27e1e9641973454b
Kostis-S-Z <kostissz@pm.me>
2025-05-09 19:11:58 +0300

    Minor updates to UI

--- source/agentic_app.py
+++ source/agentic_app.py
@@ -16,13 +16,13 @@ def _get_photo_paths_from_agent_trace(agent_trace: AgentTrace) -> List[str]:
     return list(set(paths))  # make sure there are no duplicates
 
 
-def query_agent(user_input: str) -> Tuple[List[str] | None, str]:
+def query_agent(user_input: str, instructions: str, model: str, use_web: bool) -> Tuple[List[str] | None, str]:
     # Add an event loop for the agent to run in parallel with gradio
     new_loop = asyncio.new_event_loop()
     asyncio.set_event_loop(new_loop)
 
     # Initialize the agent and run it with the user input
-    agent = init_street_view_agent()
+    agent = init_street_view_agent(instructions=instructions, model=model, use_web=use_web)
     agent_trace = agent.run(user_input)
 
     if "error" in agent_trace.final_output.lower():
@@ -34,18 +34,31 @@ def gradio_app():
     with gr.Blocks() as app:
         gr.Markdown("## The Lonely Machine")
         gr.Markdown()
+        with gr.Accordion("Configure the machine", open=False):
+            with gr.Row():
+                instructions = gr.Textbox(
+                    label="Instructions",
+                    value="You are a lonely machine that wanders the digital streets of the world.",
+                )
+            with gr.Row():
+                model = gr.Dropdown(
+                    label="Model",
+                    choices=["gpt-4.1", "gpt-4.1-mini", "gpt-4.1-nano", "o3", "o4-mini"],
+                    value="gpt-4.1",
+                )
+            with gr.Row():
+                use_web = gr.Checkbox(label="Use Web", value=False)
 
-        with gr.Row():
-            input_text = gr.Textbox(
-                label="Where should I wander?",
-                placeholder="A street, a neighborhood, a city, a country, a place...",
-            )
-            output_gallery = gr.Gallery(label="This is what I saw:")
+        input_text = gr.Textbox(
+            label="Where should I wander?",
+            placeholder="A street, a neighborhood, a city, a country, a place...",
+        )
+        output_gallery = gr.Gallery(label="This is what I saw:")
 
         status = gr.Markdown(label="Response", value="")
         submit_button = gr.Button("Wander")
         submit_button.click(
-            query_agent, inputs=input_text, outputs=[output_gallery, status]
+            query_agent, inputs=[input_text, instructions, model, use_web], outputs=[output_gallery, status]
         )
 
     app.launch()
--- source/agentic_machine.py
+++ source/agentic_machine.py
@@ -1,7 +1,10 @@
 import argparse
+import uuid
+from datetime import datetime
 from pathlib import Path
 
 from any_agent import AgentConfig, AnyAgent
+from any_agent.tools import search_web, visit_webpage
 import requests
 
 from source.the_machine.api.auth import sign_url
@@ -10,8 +13,11 @@ from source.the_machine.api.client import get_credentials
 
 # Define the Street View agent
 def init_street_view_agent(
-    framework: str = "openai", model: str = "gpt-4.1-nano"
+    instructions: str = "You are a lonely machine that wanders the digital streets of the world.", framework: str = "openai", model: str = "gpt-4.1-nano", use_web: bool = False
 ) -> AnyAgent:
+    tools = [fetch_image_from_street_view]
+    if use_web:
+        tools += [search_web, visit_webpage]
     return AnyAgent.create(
         agent_framework=framework,
         agent_config=AgentConfig(
@@ -24,13 +30,14 @@ def init_street_view_agent(
 
 # Define the Tool for the agent
 def fetch_image_from_street_view(
-    location: str, size: str, fov: int, heading: int, pitch: int, radius: int
+    coordinates: str, size: str, fov: int, heading: int, radius: int
 ) -> str:
     """
-    Fetch an image from the Google Street View API.
+    Given some coordinates in the format of XX.XXXXXX, YY.YYYYYY and other parameters fetch a photo
+    from the Google Street View API.
 
     Args:
-        location (str): Latitude and longitude in the format 'lat,lng'.
+        coordinates (str): Latitude and longitude in the format 'lat,lng' for example '37.9838,23.7275'.
         size (str): Image size in the format 'WIDTHxHEIGHT'.
         fov (int): Field of view.
         heading (int): Compass heading of the camera.
@@ -48,7 +55,7 @@ def fetch_image_from_street_view(
     base_url = "https://maps.googleapis.com/maps/api/streetview"
     url_request = (
         f"{base_url}?key={key}&size={size}"
-        f"&location={location}&radius={radius}&fov={fov}"
+        f"&location={coordinates}&radius={radius}&fov={fov}"
         f"&heading={heading}&return_error_code=true"
     )
 
@@ -57,14 +64,16 @@ def fetch_image_from_street_view(
     response = requests.get(signed_url)
 
     if response.status_code == 200:
-        location = location.replace(",", "_").replace(".", "_")
-        filename = f"streetview_{location}.jpg"
+        coordinates = coordinates.replace(",", "_").replace(".", "_")
+        timestamp = datetime.now().strftime("%d%m%Y_%H%M%S")
+        uid = str(uuid.uuid4())[:3]
+        filename = f"streetview_{timestamp}_{coordinates}_{uid}.jpg"
         full_path = output_path / filename
         with open(full_path, "wb") as file:
             file.write(response.content)
         return str(full_path)  # Return the full path
     else:
-        return f"Error when fetching image: {response.status_code} - {response.text}"
+        raise ValueError(f"Error when fetching image: {response.status_code} - {response.text}")
 
 
 if __name__ == "__main__":
